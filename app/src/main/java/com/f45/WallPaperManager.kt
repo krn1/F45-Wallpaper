ef8dc1fef5fa3f7d6e651835ca7297b987db83a4
krn1 <rkacnt@gmail.com>
2020-02-29 23:26:52 -0800

    Added glossaries

---
+++ app/src/main/java/com/f45/WallPaperManager.kt
@@ -0,0 +1,4 @@
+package com.f45
+
+class WallPaperManager {
+}
--- app/src/main/java/com/f45/WallPaperScreenActivity.kt
+++ app/src/main/java/com/f45/WallPaperScreenActivity.kt
@@ -10,15 +10,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
-class WallPaperActivity : AppCompatActivity() {
+class WallPaperScreenActivity : AppCompatActivity() {
     
-    private var wallPaperTimer: CountDownTimer? = null
     private var currentColor: Int = R.color.red
-    
-    private lateinit var currentShape: Drawable
-    private lateinit var rectangle: GradientDrawable
-    private lateinit var circle: GradientDrawable
-    private lateinit var triangle: LayerDrawable
+    private lateinit var currentWallpaper: Drawable
+    private var rotatingTimer: CountDownTimer? = null
+    private lateinit var rectangularWallpaper: GradientDrawable
+    private lateinit var circularWallpaper: GradientDrawable
+    private lateinit var triangularWallpaper: LayerDrawable
     
     // region override
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,23 +29,23 @@ class WallPaperActivity : AppCompatActivity() {
     
     override fun onStart() {
         super.onStart()
-        startWallpaperTimer()
+        startSlideShow()
     }
     
     override fun onStop() {
         super.onStop()
-        stopWallpaperTimer()
+        stopSlideShow()
     }
     // endregion
     
     // region private
     
-    private fun stopWallpaperTimer() {
-        wallPaperTimer?.cancel()
+    private fun stopSlideShow() {
+        rotatingTimer?.cancel()
     }
     
-    private fun startWallpaperTimer() {
-        wallPaperTimer = object : CountDownTimer(6 * 1000, 1000) {
+    private fun startSlideShow() {
+        rotatingTimer = object : CountDownTimer(6 * 1000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 val second = millisUntilFinished / 1000 % 60
                 timer.text = getString(R.string.timer_text, second)
@@ -63,19 +62,22 @@ class WallPaperActivity : AppCompatActivity() {
         nextShape()
         setWallPaperImage()
         
-        startWallpaperTimer()
+        startSlideShow()
     }
     
+    /**
+     *  Creates wallpaper image from current shapes and current color and displays it to the user
+     */
     private fun setWallPaperImage() {
         if (isTriangle()) {
-            val rotateDrawable = triangle
+            val rotateDrawable = triangularWallpaper
                 .findDrawableByLayerId(R.id.item_triangle).mutate() as RotateDrawable
             val drawable = rotateDrawable.drawable!!.mutate() as GradientDrawable
             
             drawable.setColor(ContextCompat.getColor(this, currentColor));
-            view.background = triangle
+            view.background = triangularWallpaper
         } else {
-            val drawable = currentShape.mutate() as GradientDrawable
+            val drawable = currentWallpaper.mutate() as GradientDrawable
             drawable.setColor(ContextCompat.getColor(this, currentColor));
             view.background = drawable
         }
@@ -94,30 +96,30 @@ class WallPaperActivity : AppCompatActivity() {
     }
     
     private fun nextShape() {
-        when (currentShape) {
+        when (currentWallpaper) {
             
-            rectangle -> currentShape = circle
-            circle -> currentShape = triangle
+            rectangularWallpaper -> currentWallpaper = circularWallpaper
+            circularWallpaper -> currentWallpaper = triangularWallpaper
             else -> {
-                currentShape = rectangle
+                currentWallpaper = rectangularWallpaper
             }
         }
     }
     
     private fun isTriangle(): Boolean {
-        return currentShape == triangle;
+        return currentWallpaper == triangularWallpaper;
     }
     
+    /**
+     *  Initializes all the shapes from the xml into drawable memory objects so it is reused for rotation
+     */
     private fun init() {
-        rectangle =
+        rectangularWallpaper =
             ContextCompat.getDrawable(this, R.drawable.rectangle)?.mutate()!! as GradientDrawable
-        circle = ContextCompat.getDrawable(this, R.drawable.circle)?.mutate()!! as GradientDrawable
-        triangle = ContextCompat.getDrawable(this, R.drawable.triangle)!!.mutate() as LayerDrawable
+        circularWallpaper = ContextCompat.getDrawable(this, R.drawable.circle)?.mutate()!! as GradientDrawable
+        triangularWallpaper = ContextCompat.getDrawable(this, R.drawable.triangle)!!.mutate() as LayerDrawable
         
-        rectangle.setColor(ContextCompat.getColor(this, currentColor));
-        view.background = rectangle
-    
-        currentShape = rectangle
+        currentWallpaper = rectangularWallpaper
     }
     // endregion
 }
