c3396c9ea395787b542f373f180bc3ee5fa7d175
krn1 <rkacnt@gmail.com>
2020-03-01 00:56:12 -0800

    updated Business Glossaries

--- app/src/main/java/com/f45/WallPaperManager.kt
+++ app/src/main/java/com/f45/WallPaperManager.kt
@@ -1,4 +1,102 @@
 package com.f45
 
-class WallPaperManager {
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.LayerDrawable
+import android.graphics.drawable.RotateDrawable
+import androidx.core.content.ContextCompat
+
+/**
+ *  This helper class  takes care of all the leg work of managing and keeping track of the
+ *  wallpaper slide show.
+ */
+class WallPaperManager(private val context: Context) {
+    
+    private var currentColor: Int = R.color.yellow
+    private var currentWallpaper: Drawable
+    
+    private val rectangularWallpaper: GradientDrawable
+    private val circularWallpaper: GradientDrawable
+    private val triangularWallpaper: LayerDrawable
+    
+    init {
+        rectangularWallpaper =
+            ContextCompat.getDrawable(context, R.drawable.rectangle)?.mutate()!! as GradientDrawable
+        circularWallpaper =
+            ContextCompat.getDrawable(context, R.drawable.circle)?.mutate()!! as GradientDrawable
+        triangularWallpaper =
+            ContextCompat.getDrawable(context, R.drawable.triangle)!!.mutate() as LayerDrawable
+        
+        currentWallpaper = triangularWallpaper
+        createNextWallPaperImage()
+    }
+    
+    // region helper methods
+    fun nextSlide() : Drawable{
+        createNextWallPaperImage()
+        return currentSlide()
+    }
+    
+    fun currentSlide(): Drawable {
+        return currentWallpaper
+    }
+    // endregion helper methods
+    
+    // region private
+    /**
+     *  Creates the next wallpaper slide from the combination of color and the shape in
+     *  a sequence
+     */
+    private fun createNextWallPaperImage() {
+        updateNextColor()
+        updateNextShape()
+        if (isTriangle()) {
+            val rotateDrawable = triangularWallpaper
+                .findDrawableByLayerId(R.id.item_triangle).mutate() as RotateDrawable
+            val drawable = rotateDrawable.drawable!!.mutate() as GradientDrawable
+            
+            drawable.setColor(ContextCompat.getColor(context, currentColor))
+            currentWallpaper = triangularWallpaper
+        } else {
+            val drawable = currentWallpaper.mutate() as GradientDrawable
+            drawable.setColor(ContextCompat.getColor(context, currentColor))
+            currentWallpaper = drawable
+        }
+    }
+    
+    /**
+     *  Updates the color in a sequence to help creating next slide
+     */
+    private fun updateNextColor() {
+        when (currentColor) {
+            R.color.red -> currentColor = R.color.green
+            R.color.green -> currentColor = R.color.blue
+            R.color.blue -> currentColor = R.color.purple
+            R.color.purple -> currentColor = R.color.yellow
+            else -> {
+                currentColor = R.color.red
+            }
+        }
+    }
+    
+    /**
+     *  Updates the shape in a sequence to create next slide
+     */
+    private fun updateNextShape() {
+        when (currentWallpaper) {
+            
+            rectangularWallpaper -> currentWallpaper = circularWallpaper
+            circularWallpaper -> currentWallpaper = triangularWallpaper
+            else -> {
+                currentWallpaper = rectangularWallpaper
+            }
+        }
+    }
+    
+    private fun isTriangle(): Boolean {
+        return currentWallpaper == triangularWallpaper;
+    }
+    
+    // endregion
 }
--- app/src/main/java/com/f45/WallPaperScreenActivity.kt
+++ app/src/main/java/com/f45/WallPaperScreenActivity.kt
@@ -1,30 +1,30 @@
 package com.f45
 
-import android.graphics.drawable.Drawable
-import android.graphics.drawable.GradientDrawable
-import android.graphics.drawable.LayerDrawable
-import android.graphics.drawable.RotateDrawable
 import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ *  Wallpaper screen responsible for showing the slide show
+ */
 class WallPaperScreenActivity : AppCompatActivity() {
     
-    private var currentColor: Int = R.color.red
-    private lateinit var currentWallpaper: Drawable
+    companion object {
+        private const val ROTATING_TIME_IN_MS = 1 * 1000L
+        private const val TIME_ONE_MS = 1 * 1000L
+        private const val TIME_SIXTY_SEC = 2 * 1000L
+    }
+    
     private var rotatingTimer: CountDownTimer? = null
-    private lateinit var rectangularWallpaper: GradientDrawable
-    private lateinit var circularWallpaper: GradientDrawable
-    private lateinit var triangularWallpaper: LayerDrawable
+    private lateinit var wallPaperManager: WallPaperManager
     
     // region override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        init()
-        setWallPaperImage()
+        wallPaperManager = WallPaperManager(this)
+        view.background = wallPaperManager.currentSlide()
     }
     
     override fun onStart() {
@@ -45,81 +45,24 @@ class WallPaperScreenActivity : AppCompatActivity() {
     }
     
     private fun startSlideShow() {
-        rotatingTimer = object : CountDownTimer(6 * 1000, 1000) {
+        rotatingTimer = object : CountDownTimer(ROTATING_TIME_IN_MS, TIME_ONE_MS) {
             override fun onTick(millisUntilFinished: Long) {
-                val second = millisUntilFinished / 1000 % 60
+                val second = millisUntilFinished / TIME_ONE_MS % TIME_SIXTY_SEC + 1L
                 timer.text = getString(R.string.timer_text, second)
             }
             
             override fun onFinish() {
-                changeWallpaper()
+                showNextSlide()
+                startSlideShow()
             }
         }.start()
     }
     
-    private fun changeWallpaper() {
-        nextColor()
-        nextShape()
-        setWallPaperImage()
-        
-        startSlideShow()
-    }
-    
     /**
-     *  Creates wallpaper image from current shapes and current color and displays it to the user
+     *  Displays the next wall paper slide
      */
-    private fun setWallPaperImage() {
-        if (isTriangle()) {
-            val rotateDrawable = triangularWallpaper
-                .findDrawableByLayerId(R.id.item_triangle).mutate() as RotateDrawable
-            val drawable = rotateDrawable.drawable!!.mutate() as GradientDrawable
-            
-            drawable.setColor(ContextCompat.getColor(this, currentColor));
-            view.background = triangularWallpaper
-        } else {
-            val drawable = currentWallpaper.mutate() as GradientDrawable
-            drawable.setColor(ContextCompat.getColor(this, currentColor));
-            view.background = drawable
-        }
-    }
-    
-    private fun nextColor() {
-        when (currentColor) {
-            R.color.red -> currentColor = R.color.green
-            R.color.green -> currentColor = R.color.blue
-            R.color.blue -> currentColor = R.color.purple
-            R.color.purple -> currentColor = R.color.yellow
-            else -> {
-                currentColor = R.color.red
-            }
-        }
-    }
-    
-    private fun nextShape() {
-        when (currentWallpaper) {
-            
-            rectangularWallpaper -> currentWallpaper = circularWallpaper
-            circularWallpaper -> currentWallpaper = triangularWallpaper
-            else -> {
-                currentWallpaper = rectangularWallpaper
-            }
-        }
-    }
-    
-    private fun isTriangle(): Boolean {
-        return currentWallpaper == triangularWallpaper;
-    }
-    
-    /**
-     *  Initializes all the shapes from the xml into drawable memory objects so it is reused for rotation
-     */
-    private fun init() {
-        rectangularWallpaper =
-            ContextCompat.getDrawable(this, R.drawable.rectangle)?.mutate()!! as GradientDrawable
-        circularWallpaper = ContextCompat.getDrawable(this, R.drawable.circle)?.mutate()!! as GradientDrawable
-        triangularWallpaper = ContextCompat.getDrawable(this, R.drawable.triangle)!!.mutate() as LayerDrawable
-        
-        currentWallpaper = rectangularWallpaper
+    private fun showNextSlide() {
+        view.background = wallPaperManager.nextSlide()
     }
     // endregion
 }
